19a988c159c3c771636ce49ffd01539dafe8f25b
kelderpassos <kelder.passos@gmail.com>
2023-01-02 16:18:54 -0300

    Feat: abstract model under development. There's a typing problem that should be fixed

---
+++ back-end/src/api/models/MongoModel.ts
@@ -0,0 +1,23 @@
+import { model, Schema } from 'mongoose';
+import { ModelInterface } from '../entities/model.interface';
+
+abstract class MongoModel<T> implements ModelInterface<T> {
+  protected _model;
+
+  constructor(alias: string, schema: Schema) {
+    this._model = model(alias, schema);
+  }
+
+  create = async (obj: T): Promise<T | null> => this._model.create({ ...obj });
+
+  readAll = async (): Promise<T[]> => this._model.find();
+
+  readOne = async (id: string): Promise<T | null> => this._model.findOne({ id });
+
+  update = async (id: string, obj: Partial<T>)
+  : Promise<T | null> => this._model.findByIdAndUpdate(id, obj);
+
+  delete = async (id: string): Promise<T | null> => this._model.findByIdAndDelete(id);
+}
+
+export { MongoModel };
