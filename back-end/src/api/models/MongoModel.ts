34b11321c877235463e4443acc78ae4d2380ab0c
kelderpassos <kelder.passos@gmail.com>
2023-01-02 19:39:23 -0300

    Refactor: added validations to Model. Now it checks if the coming id is mongo-like

--- back-end/src/api/models/MongoModel.ts
+++ back-end/src/api/models/MongoModel.ts
@@ -1,4 +1,5 @@
-import { model, Schema } from 'mongoose';
+import { model, Schema, isValidObjectId } from 'mongoose';
+import { SpecificError } from '../helpers/SpecificError';
 import { ModelInterface } from '../interfaces/api/model.interface';
 
 abstract class MongoModel<T> implements ModelInterface<T, T> {
@@ -8,17 +9,30 @@ abstract class MongoModel<T> implements ModelInterface<T, T> {
     this._model = model(alias, schema);
   }
 
+  // eslint-disable-next-line class-methods-use-this
+  private checkId = (id: string) => {
+    if (isValidObjectId(id)) throw SpecificError.invalidParameter();
+  };
+
   public create = async (obj: T)
   : Promise<T> => this._model.create({ ...obj }) as T;
 
   public readAll = async (): Promise<T[]> => this._model.find();
 
-  public readOne = async (id: string): Promise<T | null> => this._model.findOne({ id });
+  public readOne = async (id: string): Promise<T | null> => {
+    this.checkId(id);
+    return this._model.findOne({ id });
+  };
 
-  public update = async (id: string, obj: Partial<T>)
-  : Promise<T | null> => this._model.findByIdAndUpdate(id, obj);
+  public update = async (id: string, obj: Partial<T>): Promise<T | null> => {
+    this.checkId(id);
+    return this._model.findByIdAndUpdate(id, obj);
+  };
 
-  public delete = async (id: string): Promise<T | null> => this._model.findByIdAndDelete(id);
+  public delete = async (id: string): Promise<T | null> => {
+    this.checkId(id);
+    return this._model.findByIdAndDelete(id);
+  };
 }
 
 export { MongoModel };
