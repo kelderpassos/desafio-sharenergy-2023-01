9b804c8d1cb79b6be1d7d066a4ce7f018d89f1d4
kelderpassos <kelder.passos@gmail.com>
2023-01-02 16:31:58 -0300

    Fix: fixed typing problem

--- back-end/src/api/models/MongoModel.ts
+++ back-end/src/api/models/MongoModel.ts
@@ -1,14 +1,14 @@
 import { model, Schema } from 'mongoose';
 import { ModelInterface } from '../entities/model.interface';
 
-abstract class MongoModel<T> implements ModelInterface<T> {
+abstract class MongoModel<T> implements ModelInterface<T, T> {
   protected _model;
 
   constructor(alias: string, schema: Schema) {
     this._model = model(alias, schema);
   }
 
-  create = async (obj: T): Promise<T | null> => this._model.create({ ...obj });
+  create = async (obj: T): Promise<T | null> => this._model.create({ ...obj }) as unknown as T;
 
   readAll = async (): Promise<T[]> => this._model.find();
 
