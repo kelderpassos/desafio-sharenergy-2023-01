01fb3a4649da767dc6c073d3fdeeaeb15ba92049
kelderpassos <kelder.passos@gmail.com>
2023-01-02 16:10:38 -0300

    Fix: added id as a parameter for update functions

--- back-end/src/api/entities/model.interface.ts
+++ back-end/src/api/entities/model.interface.ts
@@ -2,7 +2,7 @@ interface ModelInterface<I, O> {
   create(obj: I): Promise<O | null>,
   readAll(): Promise<O[]>,
   readOne(id: string): Promise<O | null>
-  update(obj: Partial<I>): Promise<O | null>
+  update(id:string, obj: Partial<I>): Promise<O | null>
   delete(id: string): Promise<O | null>
 }
 
