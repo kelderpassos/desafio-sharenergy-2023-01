5d26261bd5709c8946dd24a9d4883cc5d421f595
kelderpassos <kelder.passos@gmail.com>
2023-01-02 21:10:15 -0300

    Feat: added error middleware and changed the kind of exportation

--- back-end/src/api/app.ts
+++ back-end/src/api/app.ts
@@ -1,8 +1,12 @@
 import express from 'express';
 import 'express-async-errors';
+import { errorHandler } from './middlewares/error';
 import { userRoute } from './routes/UserRoute';
 
 const app = express();
 
 app.use(express.json());
 app.use('/users', userRoute);
+app.use(errorHandler);
+
+export { app };
