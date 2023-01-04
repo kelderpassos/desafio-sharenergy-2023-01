a34e18d0d073578bad85a01f8d90e5fe84812455
kelderpassos <kelder.passos@gmail.com>
2023-01-04 20:33:58 -0300

    Feat: Status code page done. It still needs CSS styling

--- front-end/src/pages/StatusCodes.tsx
+++ front-end/src/pages/StatusCodes.tsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { ChangeEventHandler, useState } from 'react';
+import { Header } from '../components/Header';
 
 export const StatusCodes = () => {
+	const [code, setCode] = useState<string>('');
+
+	const handleInput: ChangeEventHandler<HTMLInputElement> = ({ target: { value } }) => {
+		setCode(value);
+	};
+
 	return (
-		<h1>teste</h1>
+		<div>
+			<Header />
+			<main>
+				<label htmlFor="codes">
+					<input type="text" id="codes" placeholder='type a code here' onChange={ handleInput }/>
+				</label>
+				<div>
+					<p>Note: not every code has an image. Those who don&apos;t will return 404</p>
+					<img src={`https://http.cat/${code}`} alt="" />
+				</div>
+			</main>
+		</div>
 	);
 };
