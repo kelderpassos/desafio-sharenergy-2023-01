b491531fd9676dd935afa0225b0425fa0bc412cb
kelderpassos <kelder.passos@gmail.com>
2023-01-05 16:45:03 -0300

    Chore: incomplete code commented so it doesn't break the application

--- front-end/src/pages/RandomUser.tsx
+++ front-end/src/pages/RandomUser.tsx
@@ -6,37 +6,80 @@ import { RandomUserType } from '../types/randomUser';
 
 export const RandomUser = () => {
 	const [search, setSearch] = useState<string>('');
-	const [randomUser, setRandomUser] = useState({} as RandomUserType);
+	const [filter, setFilter] = useState<string>('');
+	const [randomUsers, setRandomUsers] = useState<RandomUserType[]>([]);
+	const [filteredRandomUsers, setFilteredRandomUsers] = useState<RandomUserType[]>([]);
 
 	const getRandomUser = async (): Promise<void> => {
-		const randomUserData = await fetchRandomUser() as RandomUserType;		
-		setRandomUser(randomUserData);
+		const data = await fetchRandomUser();		
+		setRandomUsers(data);
 	};
 	
 	useEffect(() => {		
 		getRandomUser();
 	}, []);
 	
-	const handleInput: ChangeEventHandler<HTMLInputElement> = ({ target: { value } }) => {
-		setSearch(value);
-	};
+	// const handleInput: ChangeEventHandler<HTMLInputElement> = ({ target: { value } }) => {		
+	// 	setSearch(value);
+
+	// 	console.log(search);
+	// };
+
+	// const handleFilters: ChangeEventHandler<HTMLInputElement> = ({ target: { id } }) => {
+	// 	setFilter(id);
+	// };
+
+	
+	// const handleButton = () => {
+	// 	let filteredUser: RandomUserType[];
+	// 	switch (filter) {
+	// 	case 'name':
+	// 		filteredUser = randomUsers.filter((users) => users.name.first === filter || users.name.last === filter);
+	// 		console.log('name', filteredUser);
+			
+	// 		break;
+	// 	case 'email':
+	// 		filteredUser = randomUsers.filter((users) => users.email === filter);
+	// 		console.log('email', filteredUser);
+	// 		break;
+	// 	default:
+	// 		filteredUser = randomUsers.filter((users) => users.login === filter);
+	// 		console.log('username', filteredUser);
+	// 		break;
+	// 	}
+
+	// 	setFilteredRandomUsers(filteredUser);
+	// };
 
 	return (
 		<div>
 			<Header />
 			<main>
-				<label htmlFor="random-user">
+				{/* <label htmlFor="random-user">
 					<input type="text" id="random-user" placeholder='search user by name, email or username' onChange={ handleInput }/>
 				</label>
+				<label htmlFor="name">Name
+					<input type="radio" name="" id="name" value={filter} onChange={handleFilters} />
+				</label>
+				<label htmlFor="email">Email
+					<input type="radio" id="email" value={filter} onChange={handleFilters} />
+				</label>
+				<label htmlFor="username">Username
+					<input type="radio" name="" id="username" value={filter} onChange={handleFilters} />
+				</label>
+				<button type="submit" onClick={handleButton}>Search</button> */}
 				<div>
-					<RandomUserCard 
-						seed={randomUser.seed}
-						picture={randomUser.picture}
-						name={randomUser.name}
-						email={randomUser.email}
-						login={randomUser.login}
-						dob={randomUser.dob}
-					/>
+					{randomUsers.map(({ picture, name, email, login, dob }: RandomUserType, i) => (
+						<div key={i}>
+							<RandomUserCard								
+								picture={picture}
+								name={name}
+								email={email}
+								login={login}
+								dob={dob}
+							/>
+						</div>
+					))}
 				</div>
 			</main>
 		</div>
