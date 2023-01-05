19d733f2c879599413800acdeb0af221c5365f00
kelderpassos <kelder.passos@gmail.com>
2023-01-05 16:43:22 -0300

    Refactor: html tags changed

--- front-end/src/components/RandomUserCard.tsx
+++ front-end/src/components/RandomUserCard.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { RandomUserType } from '../types/randomUser';
 
-export const RandomUserCard = ({ picture, name, email, login, dob, seed }: RandomUserType) => {
+export const RandomUserCard = ({ picture, name, email, login, dob }: RandomUserType) => {	
+	
 	return (
-		<section key={seed}>
+		<section>
 			<img src={picture.large} alt="" />
-			<h2>{`${name.first + ' ' + name.last}`}</h2>
-			<h3>{email}</h3>
-			<h3>{login.username}</h3>
+			<h3>{`${name.first + ' ' + name.last}`}</h3>
+			<p>{email}</p>
+			<p>{login.username}</p>
 			<p>{dob.age}</p>
 		</section>
 	);
