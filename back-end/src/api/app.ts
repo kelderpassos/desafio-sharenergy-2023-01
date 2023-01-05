f90d5dca834cd560dba7e526e9dffd8034d1c31b
kelderpassos <kelder.passos@gmail.com>
2023-01-05 19:24:34 -0300

    Refactor: App.ts refactored to class object and use cors to allow connection from front-end.

--- back-end/src/api/app.ts
+++ back-end/src/api/app.ts
@@ -1,12 +1,31 @@
-import express from 'express';
 import 'express-async-errors';
+import express from 'express';
+import cors from 'cors';
 import { errorHandler } from './middlewares/error';
 import { userRoute } from './routes/UserRoute';
 
-const app = express();
+class App {
+  public app: express.Express;
 
-app.use(express.json());
-app.use('/users', userRoute);
-app.use(errorHandler);
+  constructor() {
+    this.app = express();
 
-export { app };
+    this.config();
+
+    this.app.get('/', (_req, res) => res.json('OK'));
+  }
+
+  private async config(): Promise<void> {
+    this.app.use(express.json());
+    this.app.use(cors());
+    this.app.use('/users', userRoute);
+    this.app.use(errorHandler);
+  }
+
+  public start(PORT: string | number): void {
+    // eslint-disable-next-line no-console
+    this.app.listen(PORT, () => console.log(`Running on port ${PORT}`));
+  }
+}
+
+export { App };
