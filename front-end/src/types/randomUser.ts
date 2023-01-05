4c9b95dfe55f36a1ea4a42bf1ed3d8f6f4e02d2e
kelderpassos <kelder.passos@gmail.com>
2023-01-05 20:00:46 -0300

    Feat: new type added. It types the return from database.

--- front-end/src/types/randomUser.ts
+++ front-end/src/types/randomUser.ts
@@ -1,7 +1,14 @@
-export interface RandomUserType {
+export type RandomUserType = {
   picture: Record<string, string>,
   name: Record<string, string>,
   email: string,
   login: Record<string, string>,
   dob: Record<string, string>,
+}
+
+export type UserFromDB = {
+  name: string,
+  email: string,
+  phoneNumber: number,
+  address: string, cpf: number
 }
