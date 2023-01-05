40d98b4e1b7c40ca3cdd1d6e6d402a9ed987559e
kelderpassos <kelder.passos@gmail.com>
2023-01-05 13:01:01 -0300

    Refactor: now fetchRandomUser sends exactly what the page needs. With that typing got much easier.

--- front-end/src/helpers/api.ts
+++ front-end/src/helpers/api.ts
@@ -3,7 +3,16 @@ import axios from 'axios';
 export const fetchRandomUser = async () => {
 	try {
 		const { data: { results, info: { seed } } } = await axios.get('https://randomuser.me/api/');
-		return { results, seed };
+		const userData = {
+			seed,
+			picture: results[0].picture,
+			name: results[0].name,
+			email: results[0].email,
+			login: results[0].login,
+			dob: results[0].dob
+		};
+		
+		return userData;
 	} catch (error) {
 		console.error(error);
 	}
