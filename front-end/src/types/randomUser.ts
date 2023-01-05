d3622f3990d14f4b4abb9565dda1c5788313c28f
kelderpassos <kelder.passos@gmail.com>
2023-01-05 13:03:31 -0300

    Feat: type created to set the types of the api return and the state of the page.

---
+++ front-end/src/types/randomUser.ts
@@ -0,0 +1,8 @@
+export type RandomUserType = {
+	seed: string,
+  picture: Record<string, string>,
+  name: Record<string, string>,
+  email: string,
+  login: Record<string, string>,
+  dob: Record<string, string>,
+}
