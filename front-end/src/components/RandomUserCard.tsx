8e8290b61669bcdcb3fd81858de25f5652bf4b0c
kelderpassos <kelder.passos@gmail.com>
2023-01-05 12:59:34 -0300

    Feat: RandomUserCard complete. It renders every necessary info about the user

--- front-end/src/components/RandomUserCard.tsx
+++ front-end/src/components/RandomUserCard.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { RandomUserType } from '../types/randomUser';
 
-export const RandomUserCard = ({ picture, name, email, login, dob, seed }) => {
+export const RandomUserCard = ({ picture, name, email, login, dob, seed }: RandomUserType) => {
 	return (
 		<section key={seed}>
-			<img src={picture.thumbnail} alt="" />
-			<h2>{`${name.first + '' + name.last}`}</h2>
+			<img src={picture.large} alt="" />
+			<h2>{`${name.first + ' ' + name.last}`}</h2>
 			<h3>{email}</h3>
 			<h3>{login.username}</h3>
 			<p>{dob.age}</p>
