f39390db76b4effad482613c9393ac57a2f50112
kelderpassos <kelder.passos@gmail.com>
2023-01-06 16:31:25 -0300

    Chore: Created mocks for unit tests

---
+++ back-end/src/tests/mocks/users.mocks.ts
@@ -0,0 +1,48 @@
+export const bodyMock = {
+  name: 'User',
+  email: 'user@email.com',
+  phoneNumber: 99987654321,
+  address: 'street',
+  cpf: 99999999999,
+};
+
+export const userMock = {
+  name: 'User',
+  email: 'user@email.com',
+  phoneNumber: 99987654321,
+  address: 'street',
+  cpf: 99999999999,
+  _id: '63b375808629a9d599f7e6db',
+  __v: 0,
+};
+
+// export const updatedUserMock = {
+//   name: 'User',
+//   email: 'user@email.com',
+//   phoneNumber: 99987654321,
+//   address: 'avenue',
+//   cpf: 99999999999,
+//   _id: '63b375808629a9d599f7e6db',
+//   __v: 0,
+// };
+
+export const allUsersMock = [
+  {
+    name: 'User',
+    email: 'user@email.com',
+    phoneNumber: 99987654321,
+    address: 'street',
+    cpf: 99999999999,
+    _id: '63b375808629a9d599f7e6db',
+    __v: 0,
+  },
+  {
+    name: 'Usuário',
+    email: 'usuario@email.com',
+    phoneNumber: 99987654321,
+    address: 'rua',
+    cpf: 99999999999,
+    _id: '63b375808629a9d599f7e6db',
+    __v: 0,
+  },
+];
