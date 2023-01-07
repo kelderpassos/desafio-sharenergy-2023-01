2e87dbe9cee88d7ea95cedf52009359f826d3660
kelderpassos <kelder.passos@gmail.com>
2023-01-07 19:20:11 -0300

    Refactor: changed the type of the info from the request

--- back-end/src/api/interfaces/entities/user.interface.ts
+++ back-end/src/api/interfaces/entities/user.interface.ts
@@ -3,9 +3,9 @@ import { z } from 'zod';
 const UserSchema = z.object({
   name: z.string().min(3),
   email: z.string().email(),
-  phoneNumber: z.number(),
+  phoneNumber: z.string().length(11),
   address: z.string().min(3),
-  cpf: z.number(),
+  cpf: z.string().length(11),
 });
 
 type UserInterface = z.infer<typeof UserSchema>;
