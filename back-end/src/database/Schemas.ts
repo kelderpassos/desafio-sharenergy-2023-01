576e97a511c6b3f03242230bd7f011d66a564eb3
kelderpassos <kelder.passos@gmail.com>
2023-01-07 19:21:04 -0300

    Refactor: changed the type of the data saved in the db to reflect the changes in zod validation

--- back-end/src/database/Schemas.ts
+++ back-end/src/database/Schemas.ts
@@ -4,9 +4,9 @@ import { UserInterface } from '../api/interfaces/entities/user.interface';
 const userMongooseSchema = new Schema<UserInterface>({
   name: String,
   email: String,
-  phoneNumber: Number,
+  phoneNumber: String,
   address: String,
-  cpf: Number,
+  cpf: String,
 });
 
 export { userMongooseSchema };
