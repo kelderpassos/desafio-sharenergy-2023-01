65ec30938d5d9f674abdb23988fe8236354f8ec0
kelderpassos <kelder.passos@gmail.com>
2023-01-08 15:17:54 -0300

    Chore: removing comment

--- front-end/src/components/Header.tsx
+++ front-end/src/components/Header.tsx
@@ -6,7 +6,6 @@ export const Header = () => {
 	return (
 		<header className='flex items-center justify-center	my-5'>
 			<img src={logo} alt="sharenergy's logo" className='w-[20rem] pl-5' />
-			{/* <h1 className='font-semibold text-3xl tracking-wide pl-5'>Sharenergy Challenge</h1> */}
 			<section className="flex justify-around w-full">
 				<NavLink to={'/random'} className='text-lg tracking-wide font-sans font-medium'>Random Users</NavLink>
 				<NavLink to={'/dogs'} className='text-lg tracking-wide font-sans font-medium'>Dogs</NavLink>
