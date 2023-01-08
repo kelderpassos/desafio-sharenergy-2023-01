e7ea8033ac96f3666d08fb705b7db03df018768f
kelderpassos <kelder.passos@gmail.com>
2023-01-08 18:24:40 -0300

    Style: changed margin sizes

--- front-end/src/components/Header.tsx
+++ front-end/src/components/Header.tsx
@@ -4,7 +4,7 @@ import logo from '../assets/logo_color.png';
 
 export const Header = () => {
 	return (
-		<header className='flex items-center justify-center	my-5'>
+		<header className='flex items-center justify-center	my-8'>
 			<img src={logo} alt="sharenergy's logo" className='w-[20rem] pl-5' />
 			<section className="flex justify-around w-full">
 				<NavLink to={'/random'} className='text-lg tracking-wide font-sans font-medium'>Random Users</NavLink>
