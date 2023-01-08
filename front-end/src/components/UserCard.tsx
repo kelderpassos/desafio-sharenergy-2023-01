89cbc4cea18cf1d3b81bcf751d8490c28d0b0b55
kelderpassos <kelder.passos@gmail.com>
2023-01-08 19:38:34 -0300

    Style: User Card styling finished

--- front-end/src/components/UserCard.tsx
+++ front-end/src/components/UserCard.tsx
@@ -3,11 +3,15 @@ import { UserFromDB } from '../types/userTypes';
 
 export const UserCard = ({ email, phoneNumber, address, cpf }: UserFromDB) => {	
 	return (
-		<section>
-			<p>{`Email: ${email}`}</p>
-			<p>{`Phone Number: ${phoneNumber}`}</p>
-			<p>{`Address: ${address}`}</p>
-			<p>{`CPF: ${cpf}`}</p>
+		<section className='border border-slate-300 rounded-lg flex flex-col items-center justify-center m-5 px-4'>
+			<h4 className='font-medium text-lg mt-2'>E-mail:</h4>
+			<p>{`${email}`}</p>
+			<h4 className='font-medium text-lg mt-2'>Phone Number:</h4>
+			<p>{`${phoneNumber}`}</p>
+			<h4 className='font-medium text-lg mt-2'>Address:</h4>
+			<p>{`${address}`}</p>
+			<h4 className='font-medium text-lg mt-2'>CPF:</h4>
+			<p>{`${cpf}`}</p>
 		</section>
 	);
 };
