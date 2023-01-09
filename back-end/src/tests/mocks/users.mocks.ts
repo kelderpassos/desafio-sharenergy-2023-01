18007154404f6ad41615eacb1cab7b7c31eb5998
kelderpassos <kelder.passos@gmail.com>
2023-01-09 19:26:12 -0300

    Fix: wrong types on users.mocks.ts

--- back-end/src/tests/mocks/users.mocks.ts
+++ back-end/src/tests/mocks/users.mocks.ts
@@ -1,17 +1,17 @@
 export const bodyMock = {
   name: 'User',
   email: 'user@email.com',
-  phoneNumber: 99987654321,
+  phoneNumber: '99987654321',
   address: 'street',
-  cpf: 99999999999,
+  cpf: '99999999999',
 };
 
 export const userMock = {
   name: 'User',
   email: 'user@email.com',
-  phoneNumber: 99987654321,
+  phoneNumber: '99987654321',
   address: 'street',
-  cpf: 99999999999,
+  cpf: '99999999999',
   _id: '63b375808629a9d599f7e6db',
   __v: 0,
 };
@@ -19,9 +19,9 @@ export const userMock = {
 export const invalidBodyMock = {
   name: '',
   email: '',
-  phoneNumber: 0,
+  phoneNumber: '0',
   address: '',
-  cpf: 0,
+  cpf: '0',
   _id: '',
   __v: 0,
 };
@@ -30,18 +30,18 @@ export const allUsersMock = [
   {
     name: 'User',
     email: 'user@email.com',
-    phoneNumber: 99987654321,
+    phoneNumber: '99987654321',
     address: 'street',
-    cpf: 99999999999,
+    cpf: '99999999999',
     _id: '63b375808629a9d599f7e6db',
     __v: 0,
   },
   {
     name: 'Usuário',
     email: 'usuario@email.com',
-    phoneNumber: 99987654321,
+    phoneNumber: '99987654321',
     address: 'rua',
-    cpf: 99999999999,
+    cpf: '99999999999',
     _id: '63b375808629a9d599f7e6db',
     __v: 0,
   },
