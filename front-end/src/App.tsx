319cfb1a841af36619ced8e0c94ddc83eaa67723
kelderpassos <kelder.passos@gmail.com>
2023-01-09 16:06:16 -0300

    Feat: added new functionality. Context api is now being responsible for making the api call to randomuser api and sending it to RandomUser page.

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Router } from './Router';
+import { RandomUserProvider } from './context/randomUsersProvider';
 
 export const App = () => {
 	return (
 		<BrowserRouter>
-			<Router />
+			<RandomUserProvider>
+				<Router />
+			</RandomUserProvider>
 		</BrowserRouter>
 	);
 };
