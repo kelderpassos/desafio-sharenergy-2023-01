e907365e61f46a95a2acc1868bf6076d0e992032
kelderpassos <kelder.passos@gmail.com>
2023-01-10 04:47:57 -0300

    Chore: removed unnecessary lines and importations

--- front-end/src/pages/Dogs.tsx
+++ front-end/src/pages/Dogs.tsx
@@ -1,14 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { fetchDogs } from '../helpers/api';
 import { Header } from '../components/Header';
-import { getRememberMe } from '../helpers/login';
-import { useNavigate } from 'react-router-dom';
-
 export const Dogs = () => {
 	const [dogImage, setDogimage] = useState<string | undefined>('');
 	const [loading, setLoading] = useState<boolean>(true);
 	const [refresh, setRefresh] = useState<number>();
-	const navigate = useNavigate();
 
 	const getDogsImage = async (): Promise<void> => {
 		const image = await fetchDogs();    
